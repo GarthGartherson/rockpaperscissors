7fce3cea8d90f96813a4aadc008e5583bda42e40
Garrett Greeves <garrettgreeves@gmail.com>
2021-08-09 23:16:04 +0700

    Functional Game now in console.

--- index.js
+++ index.js
@@ -1,44 +1,82 @@
-let playerPrompt = prompt('Rock Paper Scissors?').toLowerCase();
-let playerChoice = document.querySelector('.player-choice');
+// let playerChoice = document.querySelector('.player-choice');
+// let computerChoice = document.querySelector('.computer-choice');
 let result = document.querySelector('.result');
-let computerChoice = document.querySelector('.computer-choice');
+let resetButton = document.querySelector('#reset')
+let playerScore=0;
+let computerScore=0;
 
 
-const choiceArray = ['rock', 'paper', 'scissors', 'Rock', 'Paper', 'Scissors']
-while(choiceArray.includes(playerPrompt) === false){
-    playerPrompt = prompt('Rock Paper Scissors?').toLowerCase();
+
+function playRound(playerPrompt, computerRandom){
+    console.log(`Computer chooses: ${computerRandom} you chose ${playerPrompt}`)
+    if(playerPrompt === 'rock' && computerRandom === 'paper'){
+        result.innerText = 'YOU LOSE'
+        computerScore += 1;
+    } else if (playerPrompt === 'rock' && computerRandom === 'scissors'){
+        result.innerText = 'YOU WIN';
+        playerScore += 1;
+    } else if (playerPrompt === 'rock' && computerRandom === 'rock'){
+        result.innerText = 'YOU TIE'
+    }
+    if(playerPrompt === 'scissors' && computerRandom === 'rock'){
+        result.innerText = 'YOU LOSE'
+        computerScore += 1
+    } else if (playerPrompt === 'scissors' && computerRandom === 'paper'){
+        result.innerText = 'YOU WIN';
+        playerScore += 1;
+    } else if (playerPrompt === 'scissors' && computerRandom === 'scissors'){
+        result.innerText = 'YOU TIE'
+    }
+    if(playerPrompt === 'paper' && computerRandom === 'scissors'){
+        result.innerText = 'YOU LOSE'
+        computerScore += 1
+    } else if (playerPrompt === 'paper' && computerRandom === 'rock'){
+        result.innerText = 'YOU WIN';
+        playerScore += 1;
+    } else if (playerPrompt === 'paper' && computerRandom === 'paper'){
+        result.innerText = 'YOU TIE'
+    }
 }
-// if(choiceArray.includes(playerPrompt) === false) {
-//     console.log(choiceArray.includes)
+
+function getPrompt(){
+    const choiceArray = ['rock', 'paper', 'scissors', 'Rock', 'Paper', 'Scissors'];
+    const playerPrompt = prompt('Rock Paper Scissors?').toLowerCase();
+    while(choiceArray.includes(playerPrompt) === false){
+        playerPrompt = prompt('Rock Paper Scissors?').toLowerCase();
+    }
+    console.log(playerPrompt)
+    return playerPrompt;
+}
+
+function getRandom(){
+    const choiceArray = ['rock', 'paper', 'scissors', 'Rock', 'Paper', 'Scissors']
+    return computerRandom = choiceArray[(Math.floor(Math.random() * 3))]
+}
+
+function playGame(){
+    let playerPrompt = prompt('Rock Paper Scissors?').toLowerCase();
+    const choiceArray = ['rock', 'paper', 'scissors'];
+    let computerRandom = choiceArray[(Math.floor(Math.random() * 3))];    
+    while(playerScore <5 && computerScore < 5){
+        playRound(playerPrompt, computerRandom);
+        console.log(`Player: ${playerScore}`);
+        console.log(`Computer: ${computerScore}`);
+        playerPrompt = getPrompt();
+        computerRandom = getRandom();
+    }
+    if(computerScore === 5){
+        console.log('Computer Wins!')
+    } else {
+        console.log('Player Wins!')
+    }
+}
+
+playGame();
+
+// resetButton.addEventListener('click', () => {
+//     result.innerText = '';
+//     computerChoice.innerText = '';
+//     playerChoice.innerText = ''
+//     let playerScore = 0;
+//     let computerScore = 0;
 // })
-let computerRandom = choiceArray[(Math.floor(Math.random() * 3))]
-
-console.log(computerChoice)
-
-playerChoice.innerText =  playerPrompt;
-computerChoice.innerText = computerRandom 
-
-console.log(playerPrompt)
-console.log(computerRandom)
-
-if(playerPrompt === 'rock' && computerRandom === 'paper'){
-    result.innerText = 'YOU LOSE'
-} else if (playerPrompt === 'rock' && computerRandom === 'scissors'){
-    result.innerText = 'YOU WIN'
-} else if (playerPrompt === 'rock' && computerRandom === 'rock'){
-    result.innerText = 'YOU TIE'
-}
-if(playerPrompt === 'scissors' && computerRandom === 'rock'){
-    result.innerText = 'YOU LOSE'
-} else if (playerPrompt === 'scissors' && computerRandom === 'paper'){
-    result.innerText = 'YOU WIN'
-} else if (playerPrompt === 'scissors' && computerRandom === 'scissors'){
-    result.innerText = 'YOU TIE'
-}
-if(playerPrompt === 'paper' && computerRandom === 'scissors'){
-    result.innerText = 'YOU LOSE'
-} else if (playerPrompt === 'paper' && computerRandom === 'rock'){
-    result.innerText = 'YOU WIN'
-} else if (playerPrompt === 'paper' && computerRandom === 'paper'){
-    result.innerText = 'YOU TIE'
-}
